8e3697dc9a4145f2fd708ee820a403b2f4e9b16a
Gerson Lobos <gerson.lobos@whitestratus.com>
2016-09-29 22:16:33 -0400

    added tab dialogs to the md-cards for certifications.

--- WebContent/js/controllers/aboutController.js
+++ WebContent/js/controllers/aboutController.js
@@ -10,7 +10,7 @@
 
 
 angular.module('ResumeApp')
-  .controller('aboutCtrl', function ($scope) {
+  .controller('aboutCtrl', function ($scope, $mdDialog) {
     $scope.awesomeThings = [
       'gerson',
       'enrique',
@@ -19,7 +19,43 @@ angular.module('ResumeApp')
     ];
 
     $scope.imagePath = 'img/washedout.png';
-    
+    $scope.map = {
+                        center: {
+                                latitude: 56.162939,
+                                longitude: 10.203921
+                        },
+                        zoom: 8
+                };
+
+
+  $scope.showTabDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'tabDialog.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+  };
+
+    function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }  
   })
   .config(function($mdThemingProvider) {
   $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
